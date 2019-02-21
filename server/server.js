efa5e72b015bdbbfdff66e0602c1a53b7178084a
Nidhi Sharma <nidhi.x.sharma@gmail.com>
2019-02-21 13:14:36 -0800

    Add Patch/todos/:id Routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -63,6 +64,23 @@ app.post('/todos',(req,res)=>{
      }).catch((e)=>{
          res.status(400).send();
      });
+    });
+
+    app.patch('/todos/:id',(req,res)=>{
+        var id =req.params.id;
+        var body =_.pick(req.body,['text']);
+        if(!ObjectID.isValid(id)){
+            return  res.status(404).send();
+          }
+      Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+            if(!todo){
+                return res.status(404).send();
+            }
+            res.send({todo});
+      }).catch((e)=>{
+          res.status(400).send();
+      })
+
     });
 
 app.listen(port,()=>{
